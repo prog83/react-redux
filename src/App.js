a51dc46b4e996fdc7a9d849faa1e759504b1bda7
o.myronov.1607@in.np.gov.ua <o.myronov.1607@in.np.gov.ua>
2019-02-20 00:08:34 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import { createStore } from "redux";
+import { connect, Provider } from "react-redux";
 import './App.css';
+
+const initialState = {
+  fistName:   '',
+  secondName: ''
+}
+
+const ACTION_CHANGE_FIRST_NAME = 'ACTION_CHANGE_FIRST_NAME';
+const ACTION_CHANGE_SECOND_NAME = 'ACTION_CHANGE_SECOND_NAME';
+
+const actionChangeFirstName = () => {
+  return {
+    type:    ACTION_CHANGE_FIRST_NAME,
+    payload: null
+  };
+}
+
+const actionChangeSecondName = () => {
+  return {
+    type:    ACTION_CHANGE_SECOND_NAME,
+    payload: null
+  };
+}
+
+const rootReducer = (state = initialState, action) => {
+  return state;
+}
+
+const store = createStore(rootReducer);
+
+console.log(store.getState());
 
 class App extends Component {
   render() {
     return (
-      <>
-      <p>Surname</p>
-      <p>Firstname</p>
-      </>
+      <div>
+        <label>First Name <input type="text" placeholder="First Name"/></label><br/>
+        <label>Second Name <input type="text" placeholder="Second Name"/></label>        
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,88 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from "redux";
+import { connect, Provider } from "react-redux";
+import './App.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const initialState = {
+  firstName:   'Ivan',
+  secondName: 'Ivanov'
+}
+
+const ACTION_CHANGE_FIRST_NAME = 'ACTION_CHANGE_FIRST_NAME';
+const ACTION_CHANGE_SECOND_NAME = 'ACTION_CHANGE_SECOND_NAME';
+
+const changeFirstName = (newFirstName) => {
+  return {
+    type:    ACTION_CHANGE_FIRST_NAME,
+    payload: newFirstName
+  };
+}
+
+const changeSecondName = (newSecondName) => {
+  return {
+    type:    ACTION_CHANGE_SECOND_NAME,
+    payload: newSecondName
+  };
+}
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ACTION_CHANGE_FIRST_NAME:
+      return {...state, firstName: action.payload};
+    case ACTION_CHANGE_SECOND_NAME:
+      return {...state, secondName: action.payload};
+  }
+  return state;
+}
+
+const store = createStore(rootReducer);
+
+class App extends Component {
+  render() {
+    const dispatch = this.props.dispatch;
+    return (
+      <div>
+        <label>First Name 
+          <input 
+            type="text" 
+            value={this.props.firstName} 
+            onChange={(e) => {
+              dispatch(changeFirstName(e.target.value))
+            }}
+            placeholder="First Name"
+          />
+        </label><br/>
+        <label>Second Name 
+          <input 
+            type="text" 
+            value={this.props.secondName} 
+            onChange={(e) => {
+              dispatch(changeSecondName(e.target.value))
+            }}
+            placeholder="Second Name"
+          />
+        </label>        
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    firstName:  state.firstName,
+    secondName: state.secondName
+  }
+}
+
+const WrappedApp = connect(mapStateToProps)(App);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <WrappedApp />
+  </Provider>, 
+  document.getElementById('root')
+);
